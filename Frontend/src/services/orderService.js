4d39594c21c864ac88158f4b97c2226a04076174
lilyaaas <ilyasslhouari@gmail.com>
2026-05-03 21:51:15 +0100

    feat(front): add getOrderDetails method to retrieve order status and message

--- Frontend/src/services/orderService.js
+++ Frontend/src/services/orderService.js
@@ -1,13 +1,21 @@
 import api from "../api/axios";
 
 export const orderService = {
+  // Place a new Order
   placeOrder: async (orderData) => {
     const response = await api.post("/orders", orderData);
     return response.data;
   },
 
+  // Get user orders (My Orders)
   getMyOrders: async () => {
     const response = await api.get("/orders");
+    return response.data;
+  },
+
+  // Get order details (status + message)
+  getOrderDetails: async (orderId) => {
+    const response = await api.get(`/orders/${orderId}`);
     return response.data;
   },
 };
