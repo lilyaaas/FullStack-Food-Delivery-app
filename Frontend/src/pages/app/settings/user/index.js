dc48ef342086c168fb633336e681a177f023b97b
lilyaaas <ilyasslhouari@gmail.com>
2026-05-10 18:10:50 +0100

    add(front): create user settings index file to export settings components

---
+++ Frontend/src/pages/app/settings/user/index.js
@@ -0,0 +1,5 @@
+export { default as Profile } from "./Profile";
+export { default as PaymentMethods } from "./PaymentMethods";
+export { default as SavedAddresses } from "./SavedAddresses";
+export { default as Notifications } from "./Notifications";
+export { default as Security } from "./Security";
