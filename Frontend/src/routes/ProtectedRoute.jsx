a033cf23ab3a8b874ca38c9f1b2bf3dd5959e133
lilyaaas <ilyasslhouari@gmail.com>
2026-08-30 23:58:19 +0100

    feat(front): enhance ProtectedRoute to support role-based access control

--- Frontend/src/routes/ProtectedRoute.jsx
+++ Frontend/src/routes/ProtectedRoute.jsx
@@ -3,7 +3,8 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { LoadingSpinner } from "../ui/index";
 import { useAuth } from "../context/AuthContext";
 
-const ProtectedRoute = () => {
+
+const ProtectedRoute = ({ allowedRoles = [] }) => {
   const { user, isLoading } = useAuth();
   const location = useLocation();
 
@@ -11,6 +12,18 @@ const ProtectedRoute = () => {
 
   if (!user) return <Navigate to="/" state={{ from: location }} replace />;
 
+  // If route requires specific roles, check if user has the right role
+  if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
+    if (user.role === "restaurant_owner") {
+      return <Navigate to="/restaurant-owner/dashboard" replace />;
+    }
+
+    if (user.role === "customer") {
+      return <Navigate to="/" replace />;
+    }
+  }
+
+  // User is logged in and has the correct role -> Let them in!
   return <Outlet />;
 };
 
